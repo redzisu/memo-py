cd8fda3ca451e0ea5cd2e8df2490ef28f7ce90f8
redzisu <redzisu@gmail.com>
2024-07-16 18:20:05 +0900

    Feat: pyside6 기초 틀 제작
    창 열기, 파일 찾기 버튼, alert창 기능

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
-print('hello')
-print("브랜치1")
+import sys
+import os
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from mainWindow_ui import Ui_MainWindow
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        #==================  Signal  ====================
+        self.ui.btn_pathFolder.clicked.connect(self.fn_pathFolder)
+        
+    #==================  Slot ====================
+    def fn_pathFolder(self) :
+        user_documents_folder = os.path.expandvars(r'%USERPROFILE%\Documents')  # Default Root: 내 문서
+        fname = QFileDialog.getExistingDirectory(self, '폴더 선택', user_documents_folder, QFileDialog.ShowDirsOnly)
+        #self.ui.ibx_downPath.setText(fname)
+    
+    # Alert창 
+    def fn_alertBox(self, pStr): 
+        msgBox = QMessageBox()
+        msgBox.setText(pStr)
+        msgBox.exec() 
+    
+if __name__ == "__main__":
+    app = QApplication(sys.argv) 
+    window = MainWindow()        
+    window.show()                 
+    sys.exit(app.exec())         
+   
---
+++ mainWindow_ui.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainWindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QPushButton,
+    QSizePolicy, QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.btn_pathFolder = QPushButton(self.centralwidget)
+        self.btn_pathFolder.setObjectName(u"btn_pathFolder")
+        self.btn_pathFolder.setGeometry(QRect(40, 100, 75, 24))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.btn_pathFolder.setText(QCoreApplication.translate("MainWindow", u"\ud3f4\ub354 \ucc3e\uae30", None))
+    # retranslateUi
+
