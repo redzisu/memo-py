1acf72a02d03dc52297334076557d5c19b88dbce
redzisu <redzisu@gmail.com>
2024-07-17 18:24:40 +0900

    Feat: 그리드 행 추가, 삭제 추가

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
+from PySide6.QtGui import *
 from mainWindow_ui import Ui_MainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -15,26 +16,38 @@ class MainWindow(QMainWindow):
         # self.ui.grd_list.setMinimumWidth(self.ui.grd_list.sizeHintForColumn(0))
         
         #==================  Signal  ====================
-        self.ui.btn_pathFolder.clicked.connect(lambda : self.fn_confirm("삭제하시겠습니까?", lambda: print("yes"), lambda: print("no"))) 
         self.ui.btn_insertRow.clicked.connect(self.fn_insertRow)
+        self.ui.btn_deleteRow.clicked.connect(lambda : self.fn_confirm(
+            "삭제하시겠습니까?", 
+            lambda: self.fn_deleteRow(), 
+            lambda: None
+            )) 
+        
+        self.ui.grd_list.itemActivated.connect(self.fn_editRow)
         
     #==================  Slot ====================
     # 리스트 행 추가
     def fn_insertRow(self) :
-        self.ui.grd_list.addItem('')
-    
+        item = QListWidgetItem('')
+        # item.setBackground(QColor('yellow'))  # 배경색을 초록색으로 설정합니다.
+        self.ui.grd_list.addItem(item)
+        self.ui.grd_list.openPersistentEditor(item)
+        self.ui.grd_list.setCurrentItem(item)
+        
     # 리스트 행 편집
     def fn_editRow(self) :
-        user_documents_folder = os.path.expandvars(r'%USERPROFILE%\Documents')  
-        fname = QFileDialog.getExistingDirectory(self, '폴더 선택', user_documents_folder, QFileDialog.ShowDirsOnly)
-        #self.ui.ibx_downPath.setText(fname)
+        item = self.ui.grd_list.currentItem()
+        if (self.ui.grd_list.isPersistentEditorOpen(item)) :
+            self.ui.grd_list.closePersistentEditor(item)
+        else :
+            self.ui.grd_list.openPersistentEditor(item)
     
     # 리스트 행 삭제
     def fn_deleteRow(self) :
-        user_documents_folder = os.path.expandvars(r'%USERPROFILE%\Documents')  
-        fname = QFileDialog.getExistingDirectory(self, '폴더 선택', user_documents_folder, QFileDialog.ShowDirsOnly)
-        #self.ui.ibx_downPath.setText(fname)
-        
+        row = self.ui.grd_list.currentRow()
+        self.ui.grd_list.takeItem(row)
+            
+    
     # 폴더 선택창 열기 (기본경로는 '내 문서'로 설정)
     def fn_openFolder(self) :
         user_documents_folder = os.path.expandvars(r'%USERPROFILE%\Documents')  
@@ -58,13 +71,14 @@ class MainWindow(QMainWindow):
         msgBox.setIcon(QMessageBox.Question)
         msgBox.setText(pStr)
         msgBox.setStandardButtons(QMessageBox.Save | QMessageBox.Cancel)
+        msgBox.button(QMessageBox.Save).setText("확인")
+        msgBox.button(QMessageBox.Cancel).setText("취소")
         ret = msgBox.exec() 
         
         if ret == QMessageBox.Save:
             pCallback_yes()
         else:
             pCallback_no()
-            
     
 if __name__ == "__main__":
     app = QApplication(sys.argv) 
