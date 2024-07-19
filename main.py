4b4bf551e4ebead4fe9ca51bd32b4873b9bd4a17
redzisu <redzisu@gmail.com>
2024-07-19 16:16:03 +0900

    Fix: 편집창 닫을때 해당 아이템에 포커스가 가있으면 저장안되는 오류 해결
    역순으로 저장

--- main.py
+++ main.py
@@ -7,24 +7,32 @@ from PySide6.QtGui import *
 from pyqttoast import Toast, ToastPreset, ToastPosition
 
 from mainWindow_ui import Ui_MainWindow
+# This class is a custom delegate for a Qt list widget that allows for text editing with dynamic
+# height adjustment.
 
 class TextEditDelegate(QStyledItemDelegate):
+    def __init__(self, list_widget, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.list_widget = list_widget
+        
     def createEditor(self, parent, option, index):
         editor = QTextEdit(parent)
-        editor.setLineWrapMode(QTextEdit.WidgetWidth) 
-        editor.setFixedHeight(editor.fontMetrics().lineSpacing() * 3)  # 3줄 높이로 설정
         return editor
-
+    
     def setEditorData(self, editor, index):
         text = index.model().data(index, Qt.EditRole)
         editor.setPlainText(text)
-
+   
     def setModelData(self, editor, model, index):
         text = editor.toPlainText()
         model.setData(index, text, Qt.EditRole)
 
     def updateEditorGeometry(self, editor, option, index):
         editor.setGeometry(option.rect)
+
+    def sizeHint(self, option, index):
+        height = self.list_widget.viewport().height() / 3
+        return QSize(option.rect.width(), height)
         
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -38,66 +46,61 @@ class MainWindow(QMainWindow):
         # self.ui.grd_list.setMinimumWidth(self.ui.grd_list.sizeHintForColumn(0))
         
         # QListWidget 텍스트 편집 커스텀
-        self.ui.grd_list.setItemDelegate(TextEditDelegate())
+        self.ui.grd_list.setItemDelegate(TextEditDelegate(self.ui.grd_list))
         
         #==================  Signal  ====================
-        self.ui.btn_editRow.clicked.connect(self.fn_editRow)
-        self.ui.btn_insertRow.clicked.connect(self.fn_insertRow)
-        self.ui.btn_deleteRow.clicked.connect(lambda : self.fn_confirm(
+        #self.ui.btn_editList.clicked.connect(self.fn_editList)
+        self.ui.btn_insertList.clicked.connect(self.fn_insertList)
+        self.ui.btn_deleteList.clicked.connect(lambda : self.fn_confirm(
             "삭제하시겠습니까?", 
-            lambda: self.fn_deleteRow(), 
+            lambda: self.fn_deleteList(), 
             lambda: None
             )) 
         
-        self.ui.grd_list.itemClicked.connect(self.fn_copyRow)
+        self.ui.grd_list.itemDoubleClicked.connect(self.fn_editList)
+        self.ui.grd_list.itemClicked.connect(self.fn_copyList)
         self.ui.grd_list.keyPressEvent = self.event_keyPress
         
     #==================  Slot ====================
     # 리스트 행 추가
-    def fn_insertRow(self) :
+    def fn_insertList(self) :
         item = QListWidgetItem('')
         self.ui.grd_list.addItem(item)
         self.ui.grd_list.setCurrentItem(item)
         self.ui.grd_list.openPersistentEditor(item)
         
     # 리스트 행 편집
-    def fn_editRow(self) :
+    def fn_editList(self) :
         item = self.ui.grd_list.currentItem()
         if (self.ui.grd_list.isPersistentEditorOpen(item) == False) :
             self.ui.grd_list.openPersistentEditor(item)
     
     # 리스트 keyPress 이벤트
     def event_keyPress(self, event):
-        if event.key() == Qt.Key_Return :
-            self.fn_editEndRow()
+        if event.key() == Qt.Key_Return and event.modifiers() & Qt.ControlModifier:
+            self.fn_editEndList()
             
             
     # 리스트 행 편집종료
-    def fn_editEndRow(self) :
+    def fn_editEndList(self) :
         items = [self.ui.grd_list.item(x) for x in range(self.ui.grd_list.count())]
         
-        for item in items :
+        for item in reversed(items):
             if (self.ui.grd_list.isPersistentEditorOpen(item)) :
                 self.ui.grd_list.closePersistentEditor(item)
-            if (item.text().strip() == '') :
+            if not item.text().strip():
                 self.ui.grd_list.takeItem(self.ui.grd_list.row(item))
 
     # 리스트 행 삭제
-    def fn_deleteRow(self) :
+    def fn_deleteList(self) :
         row = self.ui.grd_list.currentRow()
         self.ui.grd_list.takeItem(row)
         
     # 리스트 행 복사
-    def fn_copyRow(self) :
+    def fn_copyList(self) :
         copyText = self.ui.grd_list.currentItem().text()
         clipboard.copy(copyText)
         self.fn_toast("복사 성공")
-            
-    # 폴더 선택창 열기 (기본경로는 '내 문서'로 설정)
-    def fn_openFolder(self) :
-        user_documents_folder = os.path.expandvars(r'%USERPROFILE%\Documents')  
-        fname = QFileDialog.getExistingDirectory(self, '폴더 선택', user_documents_folder, QFileDialog.ShowDirsOnly)
-        #self.ui.ibx_downPath.setText(fname)
         
     # Alert창 
     # @param 메시지
