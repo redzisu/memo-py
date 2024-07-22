38c1e5bd9568f109a022e81018b741adf50d2c5e
redzisu <redzisu@gmail.com>
2024-07-22 14:40:51 +0900

    Feat: 리스트 pickle로 저장 및 불러오기 구현

--- customStyle.py
+++ customStyle.py
@@ -1,6 +1,52 @@
 style_sheet = """
-    QListWidget::item:selected {
-        background-color: #E7F4F9;
-        color: black;
+    QMainWindow {
+        background-color: white;  /* 창 배경색 */
     }
+    
+    /* 리스트 */ 
+    QListWidget {
+        border-top: 3px double #CACACA;
+        border-bottom: 3px double #CACACA;
+        border-left: none;
+        border-right: none;
+        outline: none; /* 아이템 선택했을 때 점 테두리 제거 */
+    }
+    
+    QListWidget::item {
+        padding: 10px;
+        border-bottom: 1px dashed #CACACA;  /* 아이템 아래쪽 회색 1px 테두리 */
+    }
+    
+    QListWidget::item:hover {
+        background-color: #F1F1F1;  /* 회색 배경 */
+    }
+    
+    QListWidget::item:selected {
+        background-color: #E7F4F9;  /* 하늘색 배경 */
+        color: black;
+        border-left: 5px solid #62A668;  /* 초록색 5px 테두리 */
+    }
+    
+    /* 버튼 */
+    QPushButton {
+        background-color: #62A668;  /* 초록색 배경 */
+        color: white;  /* 흰색 텍스트 */
+        padding: 4px 16px;  /* 패딩 */
+        border-radius: 4px;  /* 둥근 모서리 */
+    }
+    
+    QPushButton:hover {
+        background-color: #57945D;  /* 조금 더 어두운 초록색 배경 */
+    }
+    
+    QPushButton:pressed {
+        background-color: #4E8454;  /* 더 어두운 초록색 배경 */
+    }
+    
+    QPushButton:disabled {
+        background-color: #D3D3D3;  /* 비활성화된 버튼 배경색 */
+        color: #A0A0A0;  /* 비활성화된 버튼 텍스트 색상 */
+        border: 2px solid #CACACA;  /* 비활성화된 버튼 테두리 */
+    }
+    
 """
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import sys
 import os
 import clipboard
+import pickle
 import customStyle
-
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 from pyqttoast import Toast, ToastPreset, ToastPosition
-
 from mainWindow_ui import Ui_MainWindow
-# This class is a custom delegate for a Qt list widget that allows for text editing with dynamic
-# height adjustment.
 
+# QListWidget 커스텀 텍스트 편집
+# LineEdit -> TextEdit : multiline 입력 및 수정 가능하도록 수정
 class TextEditDelegate(QStyledItemDelegate):
     def __init__(self, list_widget, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -22,8 +21,9 @@ class TextEditDelegate(QStyledItemDelegate):
         return editor
     
     def setEditorData(self, editor, index):
-        text = index.model().data(index, Qt.EditRole)
-        editor.setPlainText(text)
+        if self.list_widget.count() > 0 :
+            text = index.model().data(index, Qt.EditRole)
+            editor.setPlainText(text)
    
     def setModelData(self, editor, model, index):
         text = editor.toPlainText()
@@ -35,6 +35,10 @@ class TextEditDelegate(QStyledItemDelegate):
     def sizeHint(self, option, index):
         height = self.list_widget.viewport().height() / 3
         return QSize(option.rect.width(), height)
+    
+    def paint(self, painter, option, index):
+        option.displayAlignment = Qt.AlignTop | Qt.AlignLeft
+        super().paint(painter, option, index)
         
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -43,12 +47,18 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         
-        #==================  Default Setting  ====================
-        # QListWidget
-        # self.ui.grd_list.setMinimumWidth(self.ui.grd_list.sizeHintForColumn(0))
+        self.setWindowTitle("Work-Assist")
+        self.setWindowIcon(QIcon.fromTheme("emblem-favorite"))  # 아이콘 ☆
         
-        # QListWidget 텍스트 편집 커스텀
+        #==================  Default Setting  ====================
+        # QListWidget 커스텀 텍스트 편집 
         self.ui.grd_list.setItemDelegate(TextEditDelegate(self.ui.grd_list))
+        
+        # QListWidget 데이터 불러오기
+        with open("data.pickle", "rb") as f: # rb : 바이트 형식으로 읽어오기
+            data = pickle.load(f) 
+        self.ui.grd_list.insertItems(0, data)
+        
         
         #==================  Signal  ====================
         self.ui.btn_insertList.clicked.connect(self.fn_insertList)
@@ -76,11 +86,17 @@ class MainWindow(QMainWindow):
         if (self.ui.grd_list.isPersistentEditorOpen(item) == False) :
             self.ui.grd_list.openPersistentEditor(item)
     
+    # 리스트 저장
+    def fn_saveList(self) :
+        itemData = [self.ui.grd_list.item(x).text() for x in range(self.ui.grd_list.count())]
+        
+        with open("data.pickle", "wb") as f: # wb : 바이트 형식으로 쓰기
+            pickle.dump(itemData, f) 
+            
     # 리스트 keyPress 이벤트
     def event_keyPress(self, event):
         if event.key() == Qt.Key_Return and event.modifiers() & Qt.ControlModifier:
             self.fn_editEndList()
-            
             
     # 리스트 행 편집종료
     def fn_editEndList(self) :
@@ -91,11 +107,13 @@ class MainWindow(QMainWindow):
                 self.ui.grd_list.closePersistentEditor(item)
             if not item.text().strip():
                 self.ui.grd_list.takeItem(self.ui.grd_list.row(item))
-
+        self.fn_saveList()
+        
     # 리스트 행 삭제
     def fn_deleteList(self) :
         row = self.ui.grd_list.currentRow()
         self.ui.grd_list.takeItem(row)
+        self.fn_saveList()
         
     # 리스트 행 복사
     def fn_copyList(self) :
