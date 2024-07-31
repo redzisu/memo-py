c4cefcb3d9d9eecf866969cc15f69b44ea72acf9
redzisu <redzisu@gmail.com>
2024-07-31 12:59:42 +0900

    Feat: 창 크기에 따라 그리드 크기 변경

--- view_ui.py
+++ view_ui.py
@@ -18,7 +18,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QAbstractScrollArea, QApplication, QHBoxLayout, QLabel,
     QLayout, QListView, QListWidget, QListWidgetItem,
     QMainWindow, QMenuBar, QPushButton, QSizePolicy,
-    QSpacerItem, QStatusBar, QWidget)
+    QSpacerItem, QStatusBar, QVBoxLayout, QWidget)
 
 class Ui_View(object):
     def setupUi(self, View):
@@ -36,13 +36,11 @@ class Ui_View(object):
         self.centralwidget.setObjectName(u"centralwidget")
         sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
         self.centralwidget.setSizePolicy(sizePolicy)
-        self.layoutWidget = QWidget(self.centralwidget)
-        self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(10, 10, 761, 34))
-        self.horizontalLayout_2 = QHBoxLayout(self.layoutWidget)
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.label = QLabel(self.layoutWidget)
+        self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
 
         self.horizontalLayout_2.addWidget(self.label)
@@ -54,18 +52,18 @@ class Ui_View(object):
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setSizeConstraint(QLayout.SizeConstraint.SetDefaultConstraint)
-        self.btn_editList = QPushButton(self.layoutWidget)
+        self.btn_editList = QPushButton(self.centralwidget)
         self.btn_editList.setObjectName(u"btn_editList")
         self.btn_editList.setMinimumSize(QSize(0, 0))
 
         self.horizontalLayout.addWidget(self.btn_editList)
 
-        self.btn_insertList = QPushButton(self.layoutWidget)
+        self.btn_insertList = QPushButton(self.centralwidget)
         self.btn_insertList.setObjectName(u"btn_insertList")
 
         self.horizontalLayout.addWidget(self.btn_insertList)
 
-        self.btn_deleteList = QPushButton(self.layoutWidget)
+        self.btn_deleteList = QPushButton(self.centralwidget)
         self.btn_deleteList.setObjectName(u"btn_deleteList")
 
         self.horizontalLayout.addWidget(self.btn_deleteList)
@@ -73,10 +71,12 @@ class Ui_View(object):
 
         self.horizontalLayout_2.addLayout(self.horizontalLayout)
 
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
         self.grd_list = QListWidget(self.centralwidget)
         self.grd_list.setObjectName(u"grd_list")
         self.grd_list.setEnabled(True)
-        self.grd_list.setGeometry(QRect(10, 60, 761, 521))
         sizePolicy.setHeightForWidth(self.grd_list.sizePolicy().hasHeightForWidth())
         self.grd_list.setSizePolicy(sizePolicy)
         self.grd_list.setMinimumSize(QSize(100, 100))
@@ -89,6 +89,9 @@ class Ui_View(object):
         self.grd_list.setResizeMode(QListView.ResizeMode.Adjust)
         self.grd_list.setWordWrap(True)
         self.grd_list.setItemAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
+
+        self.verticalLayout.addWidget(self.grd_list)
+
         View.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(View)
         self.menubar.setObjectName(u"menubar")
