f7d0167fe574f8a16c0c54f941fef036761f4772
redzisu <redzisu@gmail.com>
2024-07-31 12:58:44 +0900

    수정전1

--- view_ui.py
+++ view_ui.py
@@ -15,16 +15,16 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractScrollArea, QApplication, QFormLayout, QHBoxLayout,
-    QLabel, QLayout, QListView, QListWidget,
-    QListWidgetItem, QMainWindow, QMenuBar, QPushButton,
-    QSizePolicy, QSpacerItem, QStatusBar, QWidget)
+from PySide6.QtWidgets import (QAbstractScrollArea, QApplication, QHBoxLayout, QLabel,
+    QLayout, QListView, QListWidget, QListWidgetItem,
+    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
+    QSpacerItem, QStatusBar, QWidget)
 
 class Ui_View(object):
     def setupUi(self, View):
         if not View.objectName():
             View.setObjectName(u"View")
-        View.resize(791, 637)
+        View.resize(915, 696)
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -36,66 +36,68 @@ class Ui_View(object):
         self.centralwidget.setObjectName(u"centralwidget")
         sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
         self.centralwidget.setSizePolicy(sizePolicy)
-        self.formLayoutWidget = QWidget(self.centralwidget)
-        self.formLayoutWidget.setObjectName(u"formLayoutWidget")
-        self.formLayoutWidget.setGeometry(QRect(10, 0, 771, 591))
-        self.formLayout = QFormLayout(self.formLayoutWidget)
-        self.formLayout.setObjectName(u"formLayout")
-        self.formLayout.setSizeConstraint(QLayout.SizeConstraint.SetDefaultConstraint)
-        self.formLayout.setFieldGrowthPolicy(QFormLayout.FieldGrowthPolicy.ExpandingFieldsGrow)
-        self.formLayout.setRowWrapPolicy(QFormLayout.RowWrapPolicy.DontWrapRows)
-        self.formLayout.setContentsMargins(0, 0, 0, 0)
-        self.label = QLabel(self.formLayoutWidget)
+        self.layoutWidget = QWidget(self.centralwidget)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(10, 10, 761, 34))
+        self.horizontalLayout_2 = QHBoxLayout(self.layoutWidget)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.layoutWidget)
         self.label.setObjectName(u"label")
 
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label)
+        self.horizontalLayout_2.addWidget(self.label)
 
-        self.grd_list = QListWidget(self.formLayoutWidget)
-        self.grd_list.setObjectName(u"grd_list")
-        self.grd_list.setStyleSheet(u"")
-        self.grd_list.setLineWidth(1)
-        self.grd_list.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
-        self.grd_list.setSizeAdjustPolicy(QAbstractScrollArea.SizeAdjustPolicy.AdjustToContents)
-        self.grd_list.setTextElideMode(Qt.TextElideMode.ElideNone)
-        self.grd_list.setResizeMode(QListView.ResizeMode.Adjust)
-        self.grd_list.setWordWrap(True)
-        self.grd_list.setItemAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
-        self.formLayout.setWidget(1, QFormLayout.SpanningRole, self.grd_list)
+        self.horizontalLayout_2.addItem(self.horizontalSpacer)
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setSizeConstraint(QLayout.SizeConstraint.SetDefaultConstraint)
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
-
-        self.horizontalLayout.addItem(self.horizontalSpacer)
-
-        self.btn_editList = QPushButton(self.formLayoutWidget)
+        self.btn_editList = QPushButton(self.layoutWidget)
         self.btn_editList.setObjectName(u"btn_editList")
         self.btn_editList.setMinimumSize(QSize(0, 0))
 
         self.horizontalLayout.addWidget(self.btn_editList)
 
-        self.btn_insertList = QPushButton(self.formLayoutWidget)
+        self.btn_insertList = QPushButton(self.layoutWidget)
         self.btn_insertList.setObjectName(u"btn_insertList")
 
         self.horizontalLayout.addWidget(self.btn_insertList)
 
-        self.btn_deleteList = QPushButton(self.formLayoutWidget)
+        self.btn_deleteList = QPushButton(self.layoutWidget)
         self.btn_deleteList.setObjectName(u"btn_deleteList")
 
         self.horizontalLayout.addWidget(self.btn_deleteList)
 
 
-        self.formLayout.setLayout(0, QFormLayout.FieldRole, self.horizontalLayout)
+        self.horizontalLayout_2.addLayout(self.horizontalLayout)
 
+        self.grd_list = QListWidget(self.centralwidget)
+        self.grd_list.setObjectName(u"grd_list")
+        self.grd_list.setEnabled(True)
+        self.grd_list.setGeometry(QRect(10, 60, 761, 521))
+        sizePolicy.setHeightForWidth(self.grd_list.sizePolicy().hasHeightForWidth())
+        self.grd_list.setSizePolicy(sizePolicy)
+        self.grd_list.setMinimumSize(QSize(100, 100))
+        self.grd_list.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self.grd_list.setStyleSheet(u"")
+        self.grd_list.setLineWidth(1)
+        self.grd_list.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        self.grd_list.setSizeAdjustPolicy(QAbstractScrollArea.SizeAdjustPolicy.AdjustIgnored)
+        self.grd_list.setTextElideMode(Qt.TextElideMode.ElideNone)
+        self.grd_list.setResizeMode(QListView.ResizeMode.Adjust)
+        self.grd_list.setWordWrap(True)
+        self.grd_list.setItemAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
         View.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(View)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 791, 22))
+        self.menubar.setGeometry(QRect(0, 0, 915, 22))
         View.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(View)
         self.statusbar.setObjectName(u"statusbar")
+        sizePolicy.setHeightForWidth(self.statusbar.sizePolicy().hasHeightForWidth())
+        self.statusbar.setSizePolicy(sizePolicy)
         View.setStatusBar(self.statusbar)
 
         self.retranslateUi(View)
